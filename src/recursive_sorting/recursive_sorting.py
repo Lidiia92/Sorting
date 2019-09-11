748d6dc84c7e98b3874306309ee9728abe877979
Lidii <lidiiany92@gmail.com>
2019-09-10 17:58:37 -0700

    Deleted comment

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -10,10 +10,14 @@ def merge( arrA, arrB ):
 
     while a_index < len(arrA) and b_index < len(arrB):
         if arrA[a_index] < arrB[b_index]:
+            print('arrA, arrB if', arrA, arrB)
             merged_arr.append(arrA[a_index])
+            print(' \n merged_array if', merged_arr)
             a_index +=1
         else:
+            print('arrA, arrB else', arrA, arrB)
             merged_arr.append(arrB[b_index])
+            print(' \n merged_array else', merged_arr)
             b_index +=1
 
     merged_arr.extend(arrB[b_index:])
@@ -21,7 +25,7 @@ def merge( arrA, arrB ):
 
     return merged_arr
 
-print(merge([1, 2, 3, 10], [0, 2, 6, 8]))
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
 
@@ -58,11 +62,3 @@ def timsort( arr ):
     return arr
 
 
-# 1. Step one:
-
-# Pass an array to the merge sort function, then I want to check for the length of array, so it is bigger than 1, so if it'so
-# bigger than 1, I want to split array into two halfs.
-
-# And If first half's length is bigger than one, I want to call merge sort function again and split 
-# arr1 into two halfs etc until my arr1 and arr2
-# will be of length 1
